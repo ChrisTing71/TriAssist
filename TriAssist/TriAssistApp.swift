ca79aeb7bd296a103bae1c8c0e09ec76108aab44
ChrisTing <dingyungwei@gmail.com>
2026-06-04 15:45:49 +0800

    Initial Commit

---
+++ TriAssist/TriAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriAssistApp.swift
+//  TriAssist
+//
+//  Created by 丁帥 on 2026/6/4.
+//
+
+import SwiftUI
+
+@main
+struct TriAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TriAssistTests/TriAssistTests.swift
@@ -0,0 +1,17 @@
+//
+//  TriAssistTests.swift
+//  TriAssistTests
+//
+//  Created by 丁帥 on 2026/6/4.
+//
+
+import Testing
+@testable import TriAssist
+
+struct TriAssistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
